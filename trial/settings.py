a5a6b48eb981c96b1a2bbaf26a502e151203c2e7
Billy Kimutai <billykimbett@gmail.com>
2023-04-04 13:03:44 +0300

    Update settings.py

--- trial/settings.py
+++ trial/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'senorfetty@gmail.com'
-EMAIL_HOST_PASSWORD = 'mojojuoeeirsltgg'
+EMAIL_HOST_USER = 'xyz@gmail.com'
+EMAIL_HOST_PASSWORD = '123456789'
